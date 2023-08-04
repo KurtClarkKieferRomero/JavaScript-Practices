fed5fa73367c0451bc5cb457f7acf3b5373d7ae6
unknown <mrdmdarius215@gmail.com>
2023-08-04 21:39:39 +0800

    Added new JS that matches Element in HTML

--- JavaScript/Sample Code/practice1JS.js
+++ JavaScript/Sample Code/practice1JS.js
@@ -1,5 +1,6 @@
-const submitButton = document.getElementById("submitButton");
-  submitButton.addEventListener("click", function () {
+const form = document.getElementById("myForm");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form from submitting normally
     const userInput = document.getElementById("textInput").value;
     console.log("You entered: " + userInput);
   });
